86a14aefa2bf7a2ffd13ccc077e63f6d95b39844
Kevin Maze <rhoxiodbc@gmail.com>
2023-08-21 01:06:23 -0700

    first commit

---
+++ lib/adjective/rpg.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rpg/version"
+
+module Adjective
+  module Rpg
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/adjective/rpg_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Adjective::Rpg do
+  it "has a version number" do
+    expect(Adjective::Rpg::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
