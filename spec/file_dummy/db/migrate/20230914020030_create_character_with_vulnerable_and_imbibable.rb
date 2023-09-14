8541f5c698b595e1bc928ff12affb61934ad3303
Kevin Maze <rhoxiodbc@gmail.com>
2023-09-14 02:10:30 -0700

    comitting before i start putting in the callable module singleton

--- spec/file_dummy/db/migrate/20230914020030_create_character_with_vulnerable_and_imbibable.rb
+++ spec/file_dummy/db/migrate/20230914020030_create_character_with_vulnerable_and_imbibable.rb
@@ -1,12 +1,13 @@
 class CreateCharacterWithVulnerableAndImbibable < ActiveRecord::Migration[7.0]
   def change
     create_table :character do |t|
-      # Adjective::Vulnerable attributes
+      # Vulnerable Attributes
       t.integer :hitpoints
       t.integer :max_hitpoints
-      # Adjective::Imbibable attributes
-      t.integer :level
+
+      # Vulnerable Attributes
       t.integer :total_experience
+      t.integer :level
     end
   end
 end
