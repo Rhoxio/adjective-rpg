1d5f42752dc8d4ae87fe4e539582a1099274cd43
Kevin Maze <rhoxiodbc@gmail.com>
2023-10-10 04:09:01 -0700

    cleaned some stuff up, ordering, privates, and comments

--- lib/modules/capacitable.rb
+++ lib/modules/capacitable.rb
@@ -1,44 +1,6 @@
 module Adjective
   module Capacitable
 
-    # Get procs working again
-
-    # restack! functionality. should restack all identity items in structs
-    # restack_item(obj), restacks a certain item signature
-    # review return values - some of them seem a little jank as defaults
-    # Add AR support - strict table declarations with correctly passed accessor method
-
-    # Resolve position collisions if they exist with AR models. Take the deepest element
-    # and just stick it in the next available nil shot. If no nil slot, throw an error
-    # Technically, this needs to work outside of Ar if they load from
-    # a text file or something, but I think having it follow AR object definitions is better.
-    # Punting until I get AR examples and figure out how I want the data in.
-
-    # Ok, so I need to set it up so the struct gets initialized 1:1 if the join table is
-    # present. If there's no join table/reference present, build from defaults (as it is now).
-    # I think I just need a different method for it. It doesnt feel clean sticking it all into 
-    # conditional statement hell there
-
-    # I have a feeling that the auto-load issue is more of an object
-    # instatiation thing. I think it needs to go into an included block?
-
-    # I think defining the AR callbacks in the included block makes sense?
-    # I could have them define methods instead of passing opts... I guess?
-    # This does make more sense. They have open options to define specific return values
-    # and can assign things directly if the need arises. As long as the method exists and returns
-    # the right value, it does make sense to do it this way versus having it be a static opts def...
-    # This even means that they can set up tables in thair db to help with this if they want to.
-    # This does make more sense, but it feels awkward. I guess it's just a matter of seeing if I can get
-    # it working correctly.
-
-    # Ok, now theres an issue with init_cap not being called. It doesn't know what defaults to use.
-    # This can be easily solved with a singleton method on the superclass that defines the data it needs.
-    # It should all be static anyway I think...
-
-    # I think the config method strategy is actaully way better - not gonna lie.
-    # The only caveat will be that any code called in it will need to be done AFTER init
-    # no matter what for the AR cases.
-
     def self.included(base)
 
       if Adjective.configuration.use_active_record
@@ -62,6 +24,27 @@ module Adjective
       end
     end
 
+    def init_capacitable(access_method, args = {}, &block)
+
+      define_capacitable_instance_variables({
+        infinite: args[:infinite] || false,
+        collection_ref: access_method,
+        max_slots: args[:max_slots] || 20,
+        baseline_weight: args[:baseline_weight] || 0,
+        stacked: args[:stacked] || false
+      })
+      
+      set_internal_variables
+     
+      if Adjective.configuration.use_active_record
+        self.build_collection!
+      else
+        self.build_simple_collection!  
+      end
+
+      yield(self) if block_given?      
+    end    
+
     def collection_origin
       self.public_send(collection_ref)
     end
@@ -84,31 +67,6 @@ module Adjective
       collection
     end
 
-    def validate_active_record_dependency_methods
-      if !self.respond_to?(:capacitable_settings)
-        raise ArgumentError, "No #capacitable_settings method defined on model. Please define it and provide a :collection_access_method, :item_accessor, and :item_collection as hash keys in the return value."
-      end
-
-      if !self.capacitable_settings.key?(:collection_access_method)
-        raise ArgumentError, "Please provide a :collection_access_method key to the #capacitable_settings method to enable ActiveRecord auto-syncing. 
-        This is the getter method for the relationship that maps how items are saved in this inventory. 
-        e.g. :bag_items method (join relationship) on a Bag model that links to the things to be stored."
-      end  
-
-      if !self.capacitable_settings.key?(:item_collection)
-        raise ArgumentError, "Please provide a :item_collection to the #capacitable_settings method to enable ActiveRecord auto-syncing.
-        This is an accessor method for the items on the model.
-        e.g. Bag has_many Items through BagItems, so :items would be correct."
-      end
-      
-      if !self.capacitable_settings.key?(:item_accessor)
-        raise ArgumentError, "Please provide a :item_accessor to the #capacitable_settings method to enable ActiveRecord auto-syncing.
-        This is an accessor method for the item in the relationship modeling. 
-        e.g. Bag has_many Items through BagItems, so :item would be correct."
-      end
-
-    end
-
     def build_collection!
       # AR version of the build and serialization.
       validate_active_record_dependency_methods
@@ -125,13 +83,14 @@ module Adjective
       })
       set_internal_variables
 
-      pulled = self.public_send(@collection_ref)
+      pulled = self.public_send(collection_ref)
       slot_count = infinite ? pulled.length + 1 : max_slots
       filled_array = Array.new(slot_count, nil)
 
       pulled.each_with_index do |data, index|
         position = (data.respond_to?(:position) && !data&.position.nil?) ? data.position : index
-        filled_array[position] = slot_struct.new(data.public_send(@item_accessor), position, 1)
+        stack_size = (data.respond_to?(:stack_size) && !data&.stack_size.nil?) ? data.stack_size : 1
+        filled_array[position] = slot_struct.new(data.public_send(item_accessor), position, stack_size)
       end
 
       self.collection = filled_array
@@ -189,10 +148,6 @@ module Adjective
         next if struct.nil?
         struct.item == object
       end      
-    end
-
-    def active_record_collection
-      # going to require some method call expectations and assumed attributes
     end
 
     def slot_struct
@@ -313,46 +268,53 @@ module Adjective
     # INTERNALS
 
     def self.default_data
-      warn("Capacitable uses array references instead of static attributes. You will need to define table values manually in your migrations.")
+      warn("Capacitable uses array references and given association accessor methods instead of static attributes. You will need to define table values manually in your migrations.")
       return {}
-    end
+    end  
 
-    def set_internal_variables
-      self.class.send(:attr_accessor, :collection)
-      self.instance_variable_set("@collection", Array.new(self.max_slots, nil))      
-    end
+    def capacitable_strategies
+      Adjective.registered_procs[:capacitable]
+    end 
 
-    def init_capacitable(access_method, args = {}, &block)
+    # PRIVATE METHODS
+    private
 
-      define_capacitable_instance_variables({
-        infinite: args[:infinite] || false,
-        collection_ref: access_method,
-        max_slots: args[:max_slots] || 20,
-        baseline_weight: args[:baseline_weight] || 0,
-        stacked: args[:stacked] || false
-      })
-      
-      set_internal_variables
-     
-      if Adjective.configuration.use_active_record
-        self.build_collection!
-      else
-        self.build_simple_collection!  
+    def validate_active_record_dependency_methods
+      if !self.respond_to?(:capacitable_settings)
+        raise NoMethodError, "No #capacitable_settings method defined on model. Please define it and provide a :collection_access_method, :item_accessor, and :item_collection as hash keys in the return value."
       end
 
-      yield(self) if block_given?      
-    end   
+      if !self.capacitable_settings.key?(:collection_access_method)
+        raise ArgumentError, "Please provide a :collection_access_method key to the #capacitable_settings method to enable ActiveRecord auto-syncing. 
+        This is the getter method for the relationship that maps how items are saved in this inventory. 
+        e.g. :bag_items method (join relationship) on a Bag model that links to the things to be stored."
+      end  
+
+      if !self.capacitable_settings.key?(:item_collection)
+        raise ArgumentError, "Please provide a :item_collection to the #capacitable_settings method to enable ActiveRecord auto-syncing.
+        This is an accessor method for the items on the model.
+        e.g. Bag has_many Items through BagItems, so :items would be correct."
+      end
+      
+      if !self.capacitable_settings.key?(:item_accessor)
+        raise ArgumentError, "Please provide a :item_accessor to the #capacitable_settings method to enable ActiveRecord auto-syncing.
+        This is an accessor method for the item in the relationship modeling. 
+        e.g. Bag has_many Items through BagItems, so :item would be correct."
+      end
+
+    end    
 
     def define_capacitable_instance_variables(args)
       args.each do |key, value|
         self.class.send(:attr_accessor, key)
         self.instance_variable_set("@#{key.to_s}", value)
       end 
-    end   
-
-    def capacitable_strategies
-      Adjective.registered_procs[:capacitable]
     end 
+
+    def set_internal_variables
+      self.class.send(:attr_accessor, :collection)
+      self.instance_variable_set("@collection", Array.new(self.max_slots, nil))      
+    end         
 
   end
 end
